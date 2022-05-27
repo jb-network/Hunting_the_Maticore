0d334b3198ff240a373505b7e5dd0e8518bdb803
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-26 22:00:51 -0500

    Add project files.

---
+++ Hunting_the_Maticore/Program.cs
@@ -0,0 +1,6 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 14 the Hunting the Manticore Challenge
+// Player 1 assumes the role of the Manticore Air Ship attacking the city of Consolas
+// Player 2 assumes the role of the defender of the city of Consolas
+
+
