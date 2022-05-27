d86c6675d2d08a2ed6eab4c74f71f3a2fdb6f29b
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-27 00:16:45 -0500

    Built play again method/local function

--- Hunting_the_Maticore/Program.cs
+++ Hunting_the_Maticore/Program.cs
@@ -3,4 +3,50 @@
 // Player 1 assumes the role of the Manticore Air Ship attacking the city of Consolas
 // Player 2 assumes the role of the defender of the city of Consolas
 
+bool playGame = true;
+
+do
+{
+
+    Console.WriteLine("Test game");
+
+    playGame = playAgain();
+} while (playGame);
+
+
+
+bool playAgain()
+{
+    int replayStatus = 0;
+    //Loop to get and validate user input 
+    do
+    {
+        Console.WriteLine("Press 1 to Play Again");
+        Console.WriteLine("Press 2 to Exit");
+        replayStatus = Convert.ToInt32(Console.ReadLine());
+        if (replayStatus <= 0 || replayStatus > 2)
+        {
+            Console.Clear();
+            Console.WriteLine($"You entered {replayStatus}, please try again");
+        }
+    }
+    while (replayStatus <= 0 || replayStatus > 2);
+
+    //either play again or not
+    if (replayStatus == 1)
+    {
+        Console.WriteLine("Press any key to restart the game");
+        Console.ReadKey();
+        Console.Clear();
+        return playGame = true;
+    }
+    else
+    {
+        Console.WriteLine("Press any key to exit the game");
+        Console.ReadKey();
+        Console.Clear();
+        return playGame = false;
+    }
+
+}
 
